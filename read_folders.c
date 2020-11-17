59b0106cdf3cf1ca948ab16e7850f7d6b5a53862
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-11-17 14:23:14 +0100

    fix recursive

--- read_folders.c
+++ read_folders.c
@@ -48,17 +48,21 @@ static int read_content(char *path, list_t **folders, int flags)
     char *filepath;
     list_t *files = NULL;
 
-    while ((fileinfo = readdir(dir))) {
-        if (fileinfo->d_name[0] == '.' && !(flags & FLAG_ALL))
-            continue;
-        filepath = my_strmerge(path, fileinfo->d_name);
-        my_strinsert(&filepath, "/", my_strlen(path));
-        if (flags & FLAG_RECURSIVE && fileinfo->d_type == DT_DIR)
-            n_folders += read_content(filepath, folders, flags);
-        add_file_to_list(filepath, fileinfo, &files);
-        free(filepath);
-    }
-    add_folder_to_list(path, files, folders);
+    if (dir != NULL) {
+        while ((fileinfo = readdir(dir))) {
+            if (fileinfo->d_name[0] == '.' && !(flags & FLAG_ALL))
+                continue;
+            filepath = my_strmerge(path, fileinfo->d_name);
+            my_strinsert(&filepath, "/", my_strlen(path));
+            printf("=> (%s)/%s - %s\n", path, fileinfo->d_name, filepath);
+            if (flags & FLAG_RECURSIVE && fileinfo->d_type == DT_DIR)
+                n_folders += read_content(filepath, folders, flags);
+            add_file_to_list(filepath, fileinfo, &files);
+            free(filepath);
+        }
+        add_folder_to_list(path, files, folders);
+    } else
+        perror(path);
     closedir(dir);
     return (n_folders);
 }
@@ -66,10 +70,15 @@ static int read_content(char *path, list_t **folders, int flags)
 int read_folders(int ac, char *av[], list_t **folders, int flags)
 {
     int n_folders = 0;
+    int len;
 
     for (int i = 1; i < ac; i++) {
-        if (*av[i] != '-')
+        if (*av[i] != '-') {
+            len = my_strlen(av[i]);
+            if (av[i][len - 1] == '/')
+                av[i][len - 1] = 0;
             n_folders += read_content(av[i], folders, flags);
+        }
     }
     return (n_folders);
 }
