79a69bb4947df36e33cce4fe3273fceafd785521
Winkler Jeffrey <wjeffrey@localhost.localdomain>
2020-11-18 14:15:01 +0100

    exec color

--- print_fcts.c
+++ print_fcts.c
@@ -13,12 +13,16 @@
 void print_color(file_t *file)
 {
     int len = my_strlen(file->name);
-    char *ext;
+    int mode = file->statbuf->st_mode;
 
     for (int i = 0; i < NUMBER_COLORS; i++) {
-        if ((file->statbuf->st_mode & S_IFMT) == COLORS_TYPE[i]) {
+        if ((mode & S_IFMT) == COLORS_TYPE[i]) {
             my_putstr(COLORS[i]);
             return;
         }
+    }
+    if (mode & S_IXUSR) {
+        my_putstr(EXEC_COLOR);
+        return;
     }
 }
