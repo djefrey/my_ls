f9865b35876f177d72d08e524261925d73466cdc
Jeffrey Winkler <jeffrey.winkler@epitech.eu>
2020-11-24 10:57:29 +0100

    add char device color

--- include/colors_list.h
+++ include/colors_list.h
@@ -10,15 +10,15 @@
 
 #include <sys/types.h>
 
-#define NUMBER_COLORS 4
+#define NUMBER_COLORS 5
 
 #define EXEC_COLOR "\e[1m\e[32m"
 #define ARCHIVE_COLOR "\e[1m\e[31m"
 
 const char* COLORS[] = {"\e[1m\e[34m", "\e[1m\e[36m",
-                    "\e[1m\e[33m", "\e[1m\e[35m"};
+                    "\e[1m\e[33m", "\e[1m\e[33m", "\e[1m\e[35m"};
 
 const int COLORS_TYPE[] = {S_IFDIR, S_IFLNK,
-                        S_IFBLK, S_IFSOCK};
+                        S_IFBLK, S_IFCHR , S_IFSOCK};
 
 #endif /* !COLORS_LIST_H_ */
